c50a94433737fdec0996f6c08ac8f2ddcc82ac5b
bold <bold@cryptoguru.com>
2020-04-09 16:41:53 +0200

    use nalgebra

--- src/deadband.rs
+++ src/deadband.rs
@@ -14,47 +14,72 @@
 /// reduce the network traffic in applications.
 ///
 /// ```rust
+/// use nalgebra::Vector3;
 /// use haptic_toolbox::DeadbandDetector;
 ///
 /// // Create a new deadband detector with a threshold of 10%.
 /// // All values that are within 10% of the previous value are considered to be
 /// // in its deadband. The initial value for deadband detection is set to 0.0.
-/// let mut deadband_detector = DeadbandDetector::new(0.1, 0.0);
+/// let mut deadband_detector = DeadbandDetector::new(0.1, Vector3::zeros());
 ///
-/// // 0.1 is not within a 10% threshold of 0.0.
-/// assert!(!deadband_detector.is_in_deadband(0.1));
-///
-/// // 0.11 is within a 10% threshold of 0.1.
-/// assert!(deadband_detector.is_in_deadband(0.11));
-///
-/// // 0.12 is not within a 10% threshold of 0.1.
-/// assert!(!deadband_detector.is_in_deadband(0.12));
-///
+/// assert!(!deadband_detector.is_in_deadband(&Vector3::new(0.1, 0.1, 0.1)));
+/// assert!(deadband_detector.is_in_deadband(&Vector3::new(0.11, 0.11, 0.11)));
+/// assert!(!deadband_detector.is_in_deadband(&Vector3::new(0.12, 0.12, 0.12)));
+/// assert!(!deadband_detector.is_in_deadband(&Vector3::new(0.0, 0.0, 0.0)));
 /// ```
+use nalgebra::Vector3;
+
 pub struct DeadbandDetector {
-    prev_val: f64,
+    prev_vals: Vector3<f64>,
     threshold: f64,
+    deadband: f64,
+}
+
+impl Default for DeadbandDetector {
+    fn default() -> Self {
+        Self::new(0.1, Vector3::zeros())
+    }
 }
 
 impl DeadbandDetector {
     /// Creates a new `DeadbandDetector`.
-    pub fn new(threshold: f64, initial_val: f64) -> Self {
-        Self {
-            prev_val: initial_val,
+    pub fn new(threshold: f64, initial_vals: Vector3<f64>) -> Self {
+        let mut deadband_detector = Self {
+            prev_vals: initial_vals,
+            deadband: 0.0,
             threshold,
-        }
+        };
+        deadband_detector.set_deadband();
+        deadband_detector
     }
 
-    /// Checks if `val` is in the deadband by comparing it to the previous
-    /// value. If `val` is outside the threshold it saves it for future
-    /// values to compare with.
-    pub fn is_in_deadband(&mut self, val: f64) -> bool {
-        let diff = val - self.prev_val;
-        if self.prev_val == 0.0 || (diff / self.prev_val).abs() > self.threshold {
-            self.prev_val = val;
+    /// Checks if `vals` are in the deadband of the previously saved vals.
+    pub fn is_in_deadband(&mut self, vals: &Vector3<f64>) -> bool {
+        let diff = (self.prev_vals - vals).norm();
+        if diff > self.deadband {
+            self.prev_vals = *vals;
+            self.set_deadband();
             false
         } else {
             true
         }
+    }
+
+    /// Sets the new deadband threshold.
+    pub fn set_threshold(&mut self, threshold: f64) {
+        assert!(
+            threshold >= 0.0 && threshold <= 1.0,
+            "cannot assign threshold outside of range [0.0, 1.0]"
+        );
+        self.threshold = threshold;
+    }
+
+    /// Returns the current deadband threshold.
+    pub fn threshold(&self) -> f64 {
+        self.threshold
+    }
+
+    fn set_deadband(&mut self) {
+        self.deadband = self.threshold * self.prev_vals.norm();
     }
 }
